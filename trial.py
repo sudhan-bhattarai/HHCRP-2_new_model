69ad8a89638aa99e7c0db99efa40cf94a33c1e99
Sudhan Bhattarai <sudhan.bhattarai26@gmail.com>
2020-10-19 15:01:52 -0600

    new constraints

--- trial.py
+++ trial.py
@@ -6,8 +6,7 @@ import math
 
 df = pd.DataFrame(pd.read_excel(r'sample_data.xlsx', 'patients'))
 df_n = pd.DataFrame(pd.read_excel(r'sample_data.xlsx', 'nurses'))
-df_n
-df_n["Time"][1]
+df.head()
 
 
 '''number of nurses, patients, days, frequency of visit, service duration & qualification'''
@@ -15,13 +14,15 @@ m = 3  # number of nurses
 t = 5  # number of days in planning horizon
 n = df.shape[0] - 1  # number of patients
 f = list(df["f"][1:].astype('int'))  # frequency of visit for every patients
+nN = list(df["nN"][1:].astype('int'))
 et = list(df["et"].astype('int'))
 lt = list(df["lt"].astype('int'))
 sd = list(df["sd"].astype('int'))
 q = list(df["Q'"][1:].astype('int'))
+q2 = list(df["Q'2"][1:].astype('int'))
+q3 = list(df["Q'3"][1:].astype('int'))
 Q = list(df_n["Q"].astype('int'))
 bigM = 1000000
-
 
 ''' coordinates X and Y of each patient and depot'''
 X, Y = list(df["x"]), list(df["y"])
@@ -104,14 +105,19 @@ M.update()
 
 '''constraint 2'''
 for j in range(n):
-    M.addConstr(gb.quicksum(x[i, j] for i in range(m)) == d[j])
+    if nN[j] == 1:
+        M.addConstr(gb.quicksum(x[i, j] for i in range(m)) == d[j])
+    else:
+        M.addConstr(gb.quicksum(x[i,j] for i in range(m)) == nN[j] * d[j])
 M.update()
 
 
 '''constraint 2'''
 for j in range(n):
-    M.addConstr(gb.quicksum(gb.quicksum(
-        y[i, j + 1, k] for k in range(t)) for i in range(m)) == f[j] * d[j])
+    if nN[j] == 1:
+        M.addConstr(gb.quicksum(gb.quicksum( y[i, j + 1, k] for k in range(t)) for i in range(m)) == f[j] * d[j])
+    else:
+        M.addConstr(gb.quicksum(gb.quicksum( y[i, j + 1, k] for k in range(t)) for i in range(m)) == nN[j] * f[j] * d[j])
 M.update()
 
 
@@ -126,8 +132,15 @@ M.update()
 '''constraint 4'''
 for i in range(m):
     for j in range(n):
-        if q[j] != Q[i]:
-            M.addConstr(x[i, j] == 0)
+        if nN[j] == 1:
+            if q[j] != Q[i]:
+                M.addConstr(x[i, j] == 0)
+        elif nN[j] == 2:
+            if q[j] != Q[i] and q2[j] != Q[i]:
+                M.addConstr(x[i, j] == 0)
+        elif nN[j] == 3:
+            if q[j] != Q[i] and q2[j] != Q[i] and q3[j] != Q[i]:
+                M.addConstr(x[i, j] == 0)
 M.update()
 
 
@@ -156,10 +169,8 @@ M.update()
 for i in range(m):
     for j in range(n + 1):
         for k in range(t):
-            M.addConstr(gb.quicksum(z[i, j, l, k]
-                                    for l in range(n + 1)) == y[i, j, k])
-            M.addConstr(gb.quicksum(z[i, l, j, k]
-                                    for l in range(n + 1)) == y[i, j, k])
+            M.addConstr(gb.quicksum(z[i, j, l, k] for l in range(n + 1)) == y[i, j, k])
+            M.addConstr(gb.quicksum(z[i, l, j, k] for l in range(n + 1)) == y[i, j, k])
             for l in range(n + 1):
                 if j == l:
                     if j != 0 and l != 0:
@@ -172,9 +183,17 @@ for i in range(m):
     for k in range(t):
         for j in range(n):
             for l in range(n):
-                M.addConstr(s[i, j + 1, k] + (sd[j + 1] + grid[j + 1, l]) *
-                            z[i, j + 1, l, k] <= s[i, l, k] + bigM * (1 - z[i, j + 1, l, k]))
+                M.addConstr(s[i, j + 1, k] + (sd[j + 1] + grid[j + 1, l]) * z[i, j + 1, l, k] <= s[i, l, k] + bigM * (1 - z[i, j + 1, l, k]))
                 M.addConstr(s[i, j, k] <= bigM * y[i, j, k])
+
+'''constraint: service start time of patient needing multiple nurses at a same time'''
+for i in range(m):
+    for p in range(m):
+        for j in range(n):
+            for k in range(t):
+                if j != 0 and nN [j] != 1 and i != p:
+                    M.addConstr(s[i, j, k] + bigM * (2 - y[i, j, k] - y[p, j, k]) >= s[p, j, k])
+                    M.addConstr(s[i, j, k] <= s[p, j, k] + bigM * (2 - y[i, j, k] - y[p, j, k]) )
 M.update()
 
 
@@ -192,10 +211,8 @@ M.update()
 for i in range(m):
     for j in range(n):
         for k in range(t):
-            M.addConstr(s[i, j + 1, k] + bigM *
-                        (1 - y[i, j + 1, k]) >= et[j + 1])
-            M.addConstr(s[i, j + 1, k] + sd[j + 1] - bigM *
-                        (1 - y[i, j + 1, k]) <= lt[j + 1])
+            M.addConstr(s[i, j + 1, k] + bigM *(1 - y[i, j + 1, k]) >= et[j + 1])
+            M.addConstr(s[i, j + 1, k] + sd[j + 1] - bigM *(1 - y[i, j + 1, k]) <= lt[j + 1])
 M.update()
 
 
